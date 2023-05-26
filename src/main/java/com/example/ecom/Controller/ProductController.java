98ad43aa893bfed1d0b8f11812a7b1d87c4fd566
Yashodhara <Yashodharagowdan@gmail.com>
2023-05-26 19:41:33 +0530

    list product

--- src/main/java/com/example/ecom/Controller/ProductController.java
+++ src/main/java/com/example/ecom/Controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.ecom.Controller;
 import com.example.ecom.Dto.CustomerDto;
 import com.example.ecom.Dto.ProductDto;
 import com.example.ecom.Dto.ProductSearchDto;
+import com.example.ecom.Service.CustomerService;
 import com.example.ecom.Service.ProductService;
 import com.example.ecom.Utility.ResponseWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,5 +22,20 @@ public class ProductController {
     public ResponseWrapper<ProductDto> searchProducts(@RequestBody ProductSearchDto productSearchDto){
         //List<ProductDto> productDtos = productService.searchProducts(productSearchDto);
         return new ResponseWrapper<>("Success","Success",200,null);
+    }
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping(value = "/register")
+    public ResponseWrapper<CustomerDto> customerRegister(@RequestBody CustomerDto customerDto){
+        customerDto = customerService.registerCustomer(customerDto);
+        return new ResponseWrapper<>("Success","Success",200,customerDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseWrapper<CustomerDto> customerLogin(@RequestBody CustomerDto customerDto){
+        customerDto = customerService.customerLogin(customerDto);
+        return new ResponseWrapper<>("Success","Success",200,customerDto);
     }
 }
