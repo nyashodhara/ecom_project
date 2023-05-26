d27a71e1799c2acf9e268254d530115d8fc0949b
Yashodhara <Yashodharagowdan@gmail.com>
2023-05-26 18:41:58 +0530

    list product

--- src/main/java/com/example/ecom/Repository/ProductRepository.java
+++ src/main/java/com/example/ecom/Repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.ecom.Repository;
 
 import com.example.ecom.Entity.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity,Long> {
 }
